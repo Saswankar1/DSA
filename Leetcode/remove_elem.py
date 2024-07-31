3cb291e1b5c9d1a1d0e697292d40228b4173164a
Saswankar Bura Gohain <111171338+Saswankar1@users.noreply.github.com>
2024-07-31 12:46:34 +0530

    Update remove_elem.py\

--- Leetcode/remove_elem.py
+++ Leetcode/remove_elem.py
@@ -20,8 +20,8 @@ k1 = solution.removeElement(nums1, val1)
 print(k1)  # Output: 2
 print(nums1[:k1])  # Output: [2, 2]
 
-nums2 = [0, 1, 2, 2, 3, 0, 4, 2]
+nums3 = [0, 1, 2, 2, 3, 0, 4, 2]
 val2 = 2
-k2 = solution.removeElement(nums2, val2)
+k2 = solution.removeElement(nums3, val2)
 print(k2)  # Output: 5
-print(nums2[:k2])  # Output: [0, 1, 3, 0, 4]
+print(nums3[:k2])  # Output: [0, 1, 3, 0, 4]
